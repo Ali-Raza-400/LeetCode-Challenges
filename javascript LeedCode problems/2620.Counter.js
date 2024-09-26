8e412d7b75d4b9d1d5b7368641a0d58fc311df8d
Ali-Raza-400 <aliraza.12748g@gmail.com>
2024-09-26 12:31:53 +0500

    solve js problem number 2620. Counter

---
+++ javascript LeedCode problems/2620.Counter.js
@@ -0,0 +1,34 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+// Example 1:
+
+// Input: 
+// n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+
+
+
+// Solution
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    let count = -1
+
+    return function () {
+        count++;
+        return n + count
+
+    };
+};
+
+//Explanation 
+
+    // createCounter(10) returns a function that increments count by 1 each time it is called.
+    // The function starts at -1, so when it is first called, it returns 10.
+    // When it is called again, count is incremented to 0, and it returns 11.
+    // This pattern continues, returning n + count each time it is called.
